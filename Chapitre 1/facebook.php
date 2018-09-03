bb3e0c9632252bb406429621dedfa7a25fd3aa88
brunazzir <34240598+brunazzir@users.noreply.github.com>
2018-09-03 10:33:20 +0200

    AGRA
    Commit of chapter 1 and 2, chapter 1 is done. First commit

---
+++ Chapitre 1/facebook.php
@@ -0,0 +1,54 @@
+<?php
+
+?>
+<html>
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="./bonjour.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <div id="logo">
+                        <img src="salut.ico" class="img-fluid"/>
+                        Centre de Formation Professionelle et Technique d'informatique
+                    </div>
+                </div> 
+                <div class="col">
+                    <div id="banner">
+                        <img src="salut.ico"/>
+                        Banner B O I S
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div id="timeline">
+                        <form id="imgupload" action="#" method="post" enc-type="multipart/form-data">
+                            <div class="row">   
+                                <div class="col-8">
+                                    <textarea id="timeline" class="form-control" rows="6" placeholder="Partagez vos pensées"></textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <input type="file" class="form-control btn btn-primary" accept="image/*" multiple/>
+                                </div>
+                                <div class="col-2">
+                                    <input type="submit" class="form-control" />
+                                </div>
+                            </div>
+                        </form>
+                        Timeline B O I S
+                        <img src="salut.ico" height="72px"/>
+                        <img src="salut.ico" class="img-fluid"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+<script src="bonjour.js"></script>
